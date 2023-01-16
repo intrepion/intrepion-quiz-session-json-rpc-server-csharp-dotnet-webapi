1c795192b3036dc6b458eeee23a98c99a8558daa
Oliver Forral <intrepion@gmail.com>
2023-01-16 11:25:57 -0700

    green - testing the health check endpoint

--- QuizApp.WebApi/HealthCheck/HealthCheckController.cs
+++ QuizApp.WebApi/HealthCheck/HealthCheckController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace QuizApp.WebApi.HealthCheck;
 
 [ApiController]
+[Route("[controller]")]
 public class HealthCheckController : ControllerBase
 {
     public IActionResult Get()
